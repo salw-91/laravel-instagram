04c2e661331dd4b95e719b2c9bfca665578118e8
Salwan-Aldulaimi <salwan@holygrow.nl>
2021-06-03 01:47:02 +0200

    Edit style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,28 +1,36 @@
 <x-app-layout>
     <x-slot name="header">
-        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2> --}}
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="row">
-                <div class="col-3 p-6">
-                    <img class="rounded-circle" style="width: 100px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F9%2F9a%2FLaravel.svg%2F1200px-Laravel.svg.png&f=1&nofb=1" alt="">
-                </div>
-                <div class="col-9">
-                        <div><h1>Laravel</h1></div>
-                    <div class="d-flex">
-                        <div class="pr-4"><strong>153</strong>posts</div>
-                        <div class="pr-4"><strong>23k</strong>followers</div>
-                        <div class="pr-4"><strong>212</strong>following</div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="row">
+                    <div class="col-3 p-6">
+                        <img class="rounded-circle" style="width: 100px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F9%2F9a%2FLaravel.svg%2F1200px-Laravel.svg.png&f=1&nofb=1" alt="">
                     </div>
-                    <div class="pt-4 font-bold">laravel.com</div>
-                    <div>Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.</div>
-                    <div><a href="https://laravel.com">www.laravel.com</a></div>
+                    <div class="col-9">
+                            <div><h1>{{ Auth::user()->username }}</h1></div>
+                        <div class="d-flex">
+                            <div class="pr-4"><strong>153</strong>posts</div>
+                            <div class="pr-4"><strong>23k</strong>followers</div>
+                            <div class="pr-4"><strong>212</strong>following</div>
+                        </div>
+                        <div class="pt-4 font-bold">laravel.com</div>
+                        <div>Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.</div>
+                        <div><a href="https://laravel.com">www.laravel.com</a></div>
+                    </div>
+                </div>
+                <div class="row pt-6">
+                    <div class="col-4">
+                        <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fheera.it%2Fwp-content%2Fuploads%2F2014%2F02%2Flaravel_smolten_2.png&f=1&nofb=1" class="" alt="">
+                    </div>
+                    <div class="col-4">
+                        <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fheera.it%2Fwp-content%2Fuploads%2F2014%2F02%2Flaravel_smolten_2.png&f=1&nofb=1" class="" alt="">
+                    </div>
+                    <div class="col-4">
+                        <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fheera.it%2Fwp-content%2Fuploads%2F2014%2F02%2Flaravel_smolten_2.png&f=1&nofb=1" class="" alt="">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </x-slot>
+
 </x-app-layout>
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -1,36 +1,37 @@
-@extends('layouts.app')
+<x-app-layout>
 
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-3 p-5">
-                <img src="/img/logo_white_Square.PNG" style="height: 100px;" class="rounded-circle">
-            </div>
-            <div class="col-9 pt-5">
-                <div class="d-flex justify-content-between align-items-baseline">
-                    <h1>{{ $user->username }}</h1>
-                    <a href="#">Add New Post</a>
-                </div>as
-                <div class="d-flex">
-                    <div class="pr-4"> <strong>150</strong>posts</div>
-                    <div class="pr-4"> <strong>15k</strong>followers</div>
-                    <div class="pr-4"> <strong>122</strong>following</div>
+    <x-slot name="header">
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-3 p-5">
+                    <img src="/img/logo_white_Square.PNG" style="height: 100px;" class="rounded-circle">
                 </div>
-                <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
-                <div>{{ $user->profile->description }}</div>
-                <div><a href="#">{{ $user->profile->url }}</a></div>
+                <div class="col-9">
+                    <div><h1>{{ Auth::user()->username }}</h1></div>
+                <div class="d-flex">
+                    <div class="pr-4"><strong>153</strong>posts</div>
+                    <div class="pr-4"><strong>23k</strong>followers</div>
+                    <div class="pr-4"><strong>212</strong>following</div>
+                </div>
+                <div class="pt-4 font-bold">laravel.com</div>
+                <div>Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.</div>
+                <div><a href="https://laravel.com">www.laravel.com</a></div>
+            </div>
+            </div>
+            <div class="row pt-5">
+                <div class="col-4">
+                    <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" class="w-100">
+                </div>
+                <div class="col-4">
+                    <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" class="w-100">
+                </div>
+                <div class="col-4">
+                    <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" class="w-100">
+                </div>
             </div>
         </div>
-        <div class="row pt-5">
-            <div class="col-4">
-                <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" class="w-100">
-            </div>
-            <div class="col-4">
-                <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" class="w-100">
-            </div>
-            <div class="col-4">
-                <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" class="w-100">
-            </div>
-        </div>
-    </div>
-@endsection
+        
+    </x-slot>
+
+</x-app-layout>
