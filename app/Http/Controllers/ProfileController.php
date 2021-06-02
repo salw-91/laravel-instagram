7560b213d1cf4f185d758a01efcf930b60ff62ed
Salwan-Aldulaimi <salwan@holygrow.nl>
2021-06-03 01:46:31 +0200

    Change in Profile Controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 class ProfileController extends Controller
 {
-    public function index($user)
+    public function index()
     {
-        $user = User::findOrfail($user);
+        $userId = Auth::user()->id;
+        $user = User::findOrfail($userId);
+        // dd($user->id);
 
         return view('profiles.index', [
             'user' => $user,
